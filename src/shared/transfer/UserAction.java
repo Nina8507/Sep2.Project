e3ee780fd49d87d53037c3e7883286dd5db4dcac
Batmanholm <61027026+Batmanholm@users.noreply.github.com>
2020-12-07 14:34:38 +0100

    Supplier

--- src/shared/transfer/UserAction.java
+++ src/shared/transfer/UserAction.java
@@ -4,10 +4,13 @@ public enum UserAction
 {
   LOGIN_REQUEST,
   LOGIN_RESULT,
+
+// Roles
   Admin,
   Secretary,
   User
-}
 // some views will be for admins only, some for secretary
 // and some for users
+}
+
 
