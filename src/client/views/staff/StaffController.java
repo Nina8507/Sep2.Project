455f98e647eccef89c596ed694c0a4f1bfe974a5
Batmanholm <holm6792@gmail.com>
2020-12-08 14:14:51 +0100

    Staff Updated

--- src/client/views/staff/StaffController.java
+++ src/client/views/staff/StaffController.java
@@ -1,5 +1,47 @@
 package client.views.staff;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.fxml.FXML;
+
 public class StaffController
 {
+  @FXML
+  private JFXButton dashboard_btn;
+
+  @FXML
+  private JFXButton product_brn;
+
+  @FXML
+  private JFXButton supplier_btn;
+
+  @FXML
+  private JFXButton orders_btn;
+
+  @FXML
+  private JFXButton staff_btn;
+
+  @FXML
+  private JFXButton cosumer_btn;
+
+  @FXML
+  private JFXButton chat_btn;
+
+  @FXML
+  private JFXButton signout_btn;
+
+  @FXML
+  private ImageView search_btn;
+
+  @FXML
+  private TextField search_staff;
+
+  @FXML
+  private JFXButton add_staff_btn;
+
+  @FXML
+  private JFXButton delete_staff_btn;
+
+  @FXML
+  private JFXButton edit_staff_btn;
+
 }
