5d88d36ba54b32481d8d3408fd953453d0510b65
Batmanholm <61027026+Batmanholm@users.noreply.github.com>
2020-12-11 18:23:55 +0100

    Staff.fxml update

--- src/client/views/supplier/SupplierController.java
+++ src/client/views/supplier/SupplierController.java
@@ -1,5 +1,39 @@
 package client.views.supplier;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+
 public class SupplierController
 {
+  public class PleaseProvideControllerClassName {
+
+    @FXML
+    private TextField searchStaffTextField;
+
+    @FXML
+    private TableView<?> supplierTable;
+
+    @FXML
+    void onAddStaffButton(ActionEvent event)
+    {
+
+    }
+
+    @FXML
+    void onDeleteStaffButton(ActionEvent event)
+    {
+
+    }
+
+    @FXML
+    void onEditStaffButton(ActionEvent event)
+    {
+
+    }
+
+    @FXML
+    void onUpdateStaffButton(ActionEvent event)
+    {
+
+    }
 }
